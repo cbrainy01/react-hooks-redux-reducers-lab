5b912e6604dc79401b080681df317820c2ce52de
Carl <carlbrainegebe@gmail.com>
2021-09-04 15:10:47 -0500

    commit tree 32160f5ed14515059b17376706f0396a244ee013

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,21 @@
 export function manageFriends(state, action) {
   // your code here
+// original state is { friends: [] }
+console.log(state)
+  switch (action.type) {
+    
+    case "friends/add": 
+    const newFriend = action.payload 
+     return { friends: [ ...state.friends, newFriend ] };
+    
+    case "friends/remove": 
+    const newFriends = state.friends.filter( (friend) => action.payload != friend.id  )
+    console.log("NEW FRIENDS: ", newFriends)
+    console.log("ACtion: ", action)
+    return { friends: newFriends } ;
+
+      
+  
+    default: return state
+  }
 }
